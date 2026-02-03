e393767bc68eabcc33def854aaea0e8c805d02d7
Cornel Gligan <cornel.ignatescu@hm.com>
2026-02-03 08:22:56 +0100

    Small refactoring

--- app/src/main/java/com/hmtest/androidnewsetup/data/utils/CountryCodeDbCallback.kt
+++ app/src/main/java/com/hmtest/androidnewsetup/data/utils/CountryCodeDbCallback.kt
@@ -3,7 +3,6 @@ package com.hmtest.androidnewsetup.data.utils
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.hmtest.androidnewsetup.data.model.CountryCodeEntity
-import com.hmtest.androidnewsetup.domain.model.CountryCode
 
 class CountryCodeDbCallback() : RoomDatabase.Callback() {
 
@@ -12,7 +11,7 @@ class CountryCodeDbCallback() : RoomDatabase.Callback() {
 
         // Build the SQL query to insert the country codes into the database
         val query = StringBuilder()
-            .append("INSERT INTO CountryCode (")
+            .append("INSERT INTO ${CountryCodeEntity.TABLE_NAME} (")
             .append(CountryCodeEntity.NUMERIC_3_CODE).append(", ")
             .append(CountryCodeEntity.ALPHA_2_CODE).append(", ")
             .append(CountryCodeEntity.ALPHA_3_CODE).append(") ")
@@ -22,13 +21,13 @@ class CountryCodeDbCallback() : RoomDatabase.Callback() {
         db.beginTransaction()
         try {
             // Insert the country codes into the database one by one
-            getMockCountryCodes().forEach { countryCode ->
+            getMockCountryCodes().forEach { countryCodeEntity ->
                 db.execSQL(
                     query,
                     arrayOf(
-                        countryCode.numeric3Code,
-                        countryCode.alpha2Code,
-                        countryCode.alpha3Code
+                        countryCodeEntity.numeric3Code,
+                        countryCodeEntity.alpha2Code,
+                        countryCodeEntity.alpha3Code
                     )
                 )
             }
@@ -38,14 +37,14 @@ class CountryCodeDbCallback() : RoomDatabase.Callback() {
         }
     }
 
-    private fun getMockCountryCodes(): List<CountryCode> = listOf(
-        CountryCode(1, "840", "US", "USA"),
-        CountryCode(2, "124", "CA", "CAN"),
-        CountryCode(3, "826", "GB", "GBR"),
-        CountryCode(4, "752", "SE", "SWE"),
-        CountryCode(5, "276", "DE", "DEU"),
-        CountryCode(6, "036", "AU", "AUS"),
-        CountryCode(7, "352", "IS", "ISL"),
-        CountryCode(8, "642", "RO", "ROU")
+    private fun getMockCountryCodes(): List<CountryCodeEntity> = listOf(
+        CountryCodeEntity(numeric3Code = "840", alpha2Code = "US", alpha3Code = "USA"),
+        CountryCodeEntity(numeric3Code = "124", alpha2Code = "CA", alpha3Code = "CAN"),
+        CountryCodeEntity(numeric3Code = "826", alpha2Code = "GB", alpha3Code = "GBR"),
+        CountryCodeEntity(numeric3Code = "752", alpha2Code = "SE", alpha3Code = "SWE"),
+        CountryCodeEntity(numeric3Code = "276", alpha2Code = "DE", alpha3Code = "DEU"),
+        CountryCodeEntity(numeric3Code = "036", alpha2Code = "AU", alpha3Code = "AUS"),
+        CountryCodeEntity(numeric3Code = "352", alpha2Code = "IS", alpha3Code = "ISL"),
+        CountryCodeEntity(numeric3Code = "642", alpha2Code = "RO", alpha3Code = "ROU")
     )
 }
